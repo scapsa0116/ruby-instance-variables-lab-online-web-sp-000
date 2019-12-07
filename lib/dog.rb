d5af51315ce426c9d5bb153153cf0b60b9876ad8
Stela Capsa <stela.capsa@gmail.com>
2019-12-07 16:38:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,12 @@
 class Dog
   def name=(dog_name)
-    this_dog_name = dog_name
+    @this_dog_name = dog_name
 end
  def name 
-   this_dog_name
+   @this_dog_name
  end
  lassie = Dog.new
  lassie.name = ("Lassie")
- 
+ puts lassie.name
  end
  
